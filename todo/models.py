46505c94c02be3f93bcc9ab54885b5b50fe2c668
lish_da_ytra <voleyballnikita@gmail.com>
2023-11-01 20:03:42 +0300

    Displaying models in

--- todo/models.py
+++ todo/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=150, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
@@ -17,5 +20,8 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
     date_end = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}, category: {self.category}'
 
 
