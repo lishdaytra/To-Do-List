f49a6cd75fcab4aaa9e0ed5167a5c0dc54e7da01
lish_da_ytra <voleyballnikita@gmail.com>
2023-11-01 19:44:52 +0300

    Create Task and Category

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    date_end = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        Category,
+        related_name='category',
+        on_delete=models.CASCADE,
+        default=0
+    )
+
