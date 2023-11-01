c88eaea47419a67e7d118bc35d89597d9d10a22e
lish_da_ytra <voleyballnikita@gmail.com>
2023-11-01 19:47:03 +0300

    Register in admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from todo.models import Task, Category
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'completed',
+        'created_at',
+        'date_end',
+        'category'
+    )
+
+admin.site.register(Task, TaskAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+admin.site.register(Category, CategoryAdmin)
 # Register your models here.
