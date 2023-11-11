2b372beb3fcb074b044c3fb05625367a19e3e209
lish_da_ytra <voleyballnikita@gmail.com>
2023-11-11 17:01:53 +0300

    Added class Meta in model Category

--- todo/models.py
+++ todo/models.py
@@ -7,6 +7,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
 
 class Task(models.Model):
     title = models.CharField(max_length=128, null=False)
